f93ac9dc0ea0ee6db20d4f192c16cb90127f9f59
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-05-21 09:33:57 +0200

    components

--- src/tabs/todo.tsx
+++ src/tabs/todo.tsx
@@ -1,11 +1,13 @@
 import "~base.css"
 import "~style.css"
 import Menu from "./menu"
+import TodoList from "./todolist"
 
 export default function Todo() {
   return (
     <div className="flex flex-col p-4">
       <Menu></Menu>
+      <TodoList></TodoList>
     </div>
   )
 }
