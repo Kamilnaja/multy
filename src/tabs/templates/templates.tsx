0fd22532fc02674bd4e93c07792eef3988cffaf1
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-05-28 14:29:53 +0200

    templates dropdown

--- src/tabs/templates/templates.tsx
+++ src/tabs/templates/templates.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import SectionDivider from "~tabs/section-divider/section-divider";
 import { templatesSelector } from "~tabs/store/selectors/template-selector";
+import TemplatesDropdown from "./templates-dropdown";
 import TemplatesTodos from "./templates-todos";
 import { type Template } from "./templates.slice";
 
@@ -36,7 +37,7 @@ function Templates() {
       key={template.id}
       className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2 ml-2">
       <div ref={menuRef} className="relative">
-        <div className="flex justify-end pt-2 absolute right-0">
+        <div className="flex justify-end absolute right-0">
           <button
             id="dropdownButton"
             data-dropdown-toggle="dropdown"
@@ -54,34 +55,7 @@ function Templates() {
             </svg>
           </button>
           {open.id === template.id && open.isOpened && (
-            <div
-              id="dropdown"
-              className="z-10 absolute text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 top-0 right-10">
-              <ul className="py-2" aria-labelledby="dropdownButton">
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Add to todos
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Edit
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Archive
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <TemplatesDropdown></TemplatesDropdown>
           )}
         </div>
         <div>
