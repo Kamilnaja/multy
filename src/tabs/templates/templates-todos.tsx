752a4e79b998dfaba9d42660da53f9e7f5e26c8c
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-05-28 11:05:50 +0200

    displaying templates

---
+++ src/tabs/templates/templates-todos.tsx
@@ -0,0 +1,28 @@
+import type { Template } from "./templates.slice";
+
+type TemplatesTodosProps = {
+  template: Template;
+};
+
+export default function TemplatesTodos({ template }: TemplatesTodosProps) {
+  return (
+    <>
+      <div className="flex items-center justify-between mt-4 text-gray-900 dark:text-white mb-2">
+        Todos
+      </div>
+      <ul>
+        {Object.values(template.todos).map((todo) => (
+          <li
+            className="flex border p-4 dark:text-gray-400"
+            key={`${todo.id}-${template.id}`}>
+            <div>
+              <h3 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
+                {todo.description}
+              </h3>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
--- src/tabs/templates/templates.tsx
+++ src/tabs/templates/templates.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { templatesSelector } from "~tabs/store/selectors/template-selector";
+import TemplatesTodos from "./templates-todos";
 import { type Template } from "./templates.slice";
 
 function Templates() {
@@ -11,9 +12,6 @@ function Templates() {
       className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2 ml-2">
       <div>
         <div>
-          <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {template.id}
-          </h2>
           <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">
             {template.title}
           </h3>
@@ -21,23 +19,7 @@ function Templates() {
             {template.description}
           </span>
         </div>
-        <div className="flex items-center justify-between mt-4 text-gray-900 dark:text-white mb-2">
-          Tasks
-        </div>
-        <ul>
-          {Object.values(template.todos).map((todo) => (
-            <li
-              className="flex border p-4 dark:text-gray-400"
-              key={`${todo.id}-${template.id}`}>
-              <div>
-                <span>{JSON.stringify(todo)}</span>
-                <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {todo.description}
-                </h3>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <TemplatesTodos template={template}></TemplatesTodos>
       </div>
     </li>
   ));
