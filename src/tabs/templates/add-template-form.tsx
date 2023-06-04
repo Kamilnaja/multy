e1b09847d65abc70630f2054b4ad579d8ceadc3f
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-06-04 21:51:38 +0200

    title change

--- src/tabs/templates/add-template-form.tsx
+++ src/tabs/templates/add-template-form.tsx
@@ -40,13 +40,13 @@ const AddTemplateForm = () => {
 
   return (
     <form onSubmit={handleSubmit} className="mx-auto">
-      <label htmlFor="name" className="block mb-2">
-        Name:
+      <label htmlFor="title" className="block mb-2">
+        Title:
       </label>
       <input
         type="text"
-        id="name"
-        name="name"
+        id="title"
+        name="title"
         value={template.title}
         onChange={handleChange}
         className="w-full border border-gray-300 rounded-md py-2 px-3 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
